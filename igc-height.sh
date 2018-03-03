105ae469f7d98c4cc11402cf5cfae18d2e1e7f0e
Yaasiin Oozeer <yaasiinoozeer@gmail.com>
2018-03-03 16:43:01 -0500

    height script

--- igc-height.sh
+++ igc-height.sh
@@ -1,1 +1,12 @@
- grep ^B $1 | cut -c 31-35 | sort | tail -n 1
+# time=161223
+# timeHH=16
+# timeMM=12
+# timeSS=23
+# echo 16:12:23
+
+time=$(grep ^B $1 | cut -c 2-7 | head -n 1)
+timeHH=${time:0:2}
+timeMM=${time:2:2}
+timeSS=${time:4:2}
+
+echo $timeHH:$timeMM:$timeSS
